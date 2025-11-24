ceabf0194dcf823de909224c80241f54b674bace
OtavioAlves-pro <otavioalvesp1souza@gmail.com>
2025-11-24 20:05:40 -0300

    Add files via upload

---
+++ While/Numeros.java
@@ -0,0 +1,18 @@
+package laco2;
+
+import java.util.Scanner;
+
+public class laco {
+
+	public static void main (String [] args) {
+		
+		int i = 1;
+  
+        while (i < 1001) {
+            System.out.print (i +" " );
+            i += 2;
+        }
+    }
+}
+
+	
