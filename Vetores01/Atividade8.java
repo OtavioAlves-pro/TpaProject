b0a6aef872510198c2e35d6306b53248373cd7cd
OtavioAlves-pro <otavioalvesp1souza@gmail.com>
2025-12-02 00:30:58 -0300

    Fix cumulative sum calculation in Elementosfat
    Reset the sum variable for each iteration to calculate the cumulative sum correctly.

--- Vetores01/Atividade8.java
+++ Vetores01/Atividade8.java
@@ -1,42 +1,36 @@
-
 import java.util.Scanner;
 
 public class Elementosfat {
 	
-		public static void main(String[] args) {
-	        Scanner ler = new Scanner(System.in);
-			
-	        final int TAM=10;
-	        int a[], b[],  i, j, soma = 0;
-			
-	        a = new int [TAM];
-	        b = new int [TAM];
-	        System.out.println("Digite 10 elementos inteiros do vetor A:");
-	        for ( i = 0; i < TAM; i++) {
-	            a[i] = ler.nextInt();
-	        }
-	       
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+		
+        final int TAM=10;
+        int a[], b[],  i, j, soma = 0;
+		
+        a = new int [TAM];
+        b = new int [TAM];
+        System.out.println("Digite 10 elementos inteiros do vetor A:");
+        for ( i = 0; i < TAM; i++) {
+            a[i] = ler.nextInt();
+        }
 
-	        for ( i = 0; i < TAM; i++) {
-	            for ( j = i; j < TAM; j++) {
-	                soma += a[j];
-	            }
-	            b[i] = soma;
-	        }
-			
-	        System.out.println("Vetor A:");
-	        for ( i = 0; i < TAM; i++) {
-	            System.out.print(a[i] + " ");
-	        }
-			
-	        System.out.println("\n\Vetor B (soma):");
-	        for ( i = 0; i < TAM; i++) {
-	            System.out.println("B[" + i + "] = " + b[i]);
-	        }
-	    }
-	}
-
-
-
-
-
+        for ( i = 0; i < TAM; i++) {
+            soma = 0; 
+            for ( j = i; j < TAM; j++) {
+                soma += a[j];
+            }
+            b[i] = soma;
+        }
+		
+        System.out.println("Vetor A:");
+        for ( i = 0; i < TAM; i++) {
+            System.out.print(a[i] + " ");
+        }
+		
+        System.out.println("\nVetor B (soma):");
+        for ( i = 0; i < TAM; i++) {
+            System.out.println("B[" + i + "] = " + b[i]);
+        }
+    }
+}
